0fa17a285efd381269b0bb9899bd88b324daba3c
Russ Wirtz <rwirtz@zipwhip.com>
2013-08-20 11:19:44 -0700

    Added ObjectPooling to ScheduledRunnable (rather than Map).

--- src/main/java/com/corundumstudio/socketio/ack/AckManager.java
+++ src/main/java/com/corundumstudio/socketio/ack/AckManager.java
@@ -23,6 +23,11 @@ import com.corundumstudio.socketio.scheduler.CancelableScheduler;
 import com.corundumstudio.socketio.scheduler.SchedulerKey;
 import com.corundumstudio.socketio.scheduler.SchedulerKey.Type;
 import com.corundumstudio.socketio.transport.BaseClient;
+import com.google.common.base.Preconditions;
+import com.zipwhip.pools.PoolUtil;
+import com.zipwhip.pools.PoolableObjectFactoryBase;
+import org.apache.commons.pool.ObjectPool;
+import org.apache.commons.pool.PoolUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -34,36 +39,13 @@ import java.util.concurrent.atomic.AtomicLong;
 
 public class AckManager implements Disconnectable {
 
-    class AckEntry {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AckManager.class);
 
-        final Map<Long, AckCallback<?>> ackCallbacks = new ConcurrentHashMap<Long, AckCallback<?>>();
-        final AtomicLong ackIndex = new AtomicLong(-1);
-
-        public long addAckCallback(AckCallback<?> callback) {
-            long index = ackIndex.incrementAndGet();
-            ackCallbacks.put(index, callback);
-            return index;
+    protected static final ObjectPool OBJECT_POOL = PoolUtils.erodingPool(PoolUtil.getPool(new PoolableObjectFactoryBase() {
+        public Object makeObject() throws Exception {
+            return new ScheduledRunnable();
         }
-
-        public Set<Long> getAckIndexes() {
-            return ackCallbacks.keySet();
-        }
-
-        public AckCallback<?> getAckCallback(long index) {
-            return ackCallbacks.get(index);
-        }
-
-        public AckCallback<?> removeCallback(long index) {
-            return ackCallbacks.remove(index);
-        }
-
-        public void initAckIndex(long index) {
-            ackIndex.compareAndSet(-1, index);
-        }
-
-    }
-
-    private final Logger log = LoggerFactory.getLogger(getClass());
+    }));
 
     private final Map<String, AckEntry> ackEntries = new ConcurrentHashMap<String, AckEntry>();
 
@@ -95,7 +77,7 @@ public class AckManager implements Disconnectable {
         SchedulerKey key = new SchedulerKey(Type.ACK_TIMEOUT, client.getSessionId());
         scheduler.cancel(key);
 
-        AckCallback callback = removeCallback(client.getSessionId(), packet.getAckId());
+        AckCallback callback = removeCallback(ackEntries, client.getSessionId(), packet.getAckId());
         if (callback != null) {
             Object param = null;
             if (!packet.getArgs().isEmpty()) {
@@ -105,7 +87,7 @@ public class AckManager implements Disconnectable {
         }
     }
 
-    private AckCallback removeCallback(String sessionId, long index) {
+    private static AckCallback removeCallback(Map<String, AckEntry> ackEntries, String sessionId, long index) {
         AckEntry ackEntry = ackEntries.get(sessionId);
         // may be null if client disconnected
         // before timeout occurs
@@ -125,8 +107,8 @@ public class AckManager implements Disconnectable {
         ackEntry.initAckIndex(0);
         long index = ackEntry.addAckCallback(callback);
 
-        if (log.isDebugEnabled()) {
-            log.debug("AckCallback registered with id: {}", index);
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("AckCallback registered with id: {}", index);
         }
 
         scheduleTimeout(index, sessionId, callback);
@@ -138,14 +120,40 @@ public class AckManager implements Disconnectable {
         if (callback.getTimeout() == -1) {
             return;
         }
+        // TODO: consider adding ObjectPooling to AckSchedulerKey
         SchedulerKey key = new AckSchedulerKey(Type.ACK_TIMEOUT, sessionId, index);
-        scheduler.schedule(key, new Runnable() {
-            @Override
-            public void run() {
-                removeCallback(sessionId, index);
-                callback.onTimeout();
-            }
-        }, callback.getTimeout(), TimeUnit.SECONDS);
+        ScheduledRunnable runnable = borrowRunnable(callback, key.getSessionId(), index);
+
+        try {
+            scheduler.schedule(key, runnable, callback.getTimeout(), TimeUnit.SECONDS);
+        } catch (Exception e) {
+            releaseScheduledRunnable(runnable);
+        }
+    }
+
+    private static void releaseScheduledRunnable(ScheduledRunnable runnable) {
+        try {
+            OBJECT_POOL.returnObject(runnable);
+        } catch (Exception e) {
+            LOGGER.error("Failed to return runnable", e);
+        }
+    }
+
+    private ScheduledRunnable borrowRunnable(AckCallback callback, String sessionId, long index) {
+        ScheduledRunnable runnable;
+
+        try {
+            runnable = (ScheduledRunnable) OBJECT_POOL.borrowObject();
+        } catch (Exception e) {
+            runnable = new ScheduledRunnable();
+        }
+
+        runnable.setAckEntries(ackEntries);
+        runnable.setCallback(callback);
+        runnable.setIndex(index);
+        runnable.setSessionId(sessionId);
+
+        return runnable;
     }
 
     @Override
@@ -153,4 +161,85 @@ public class AckManager implements Disconnectable {
         ackEntries.remove(client.getSessionId());
     }
 
+    private static class ScheduledRunnable implements Runnable {
+
+        private String sessionId;
+        private long index;
+        private AckCallback callback;
+        private Map<String, AckEntry> ackEntries;
+
+        @Override
+        public void run() {
+            try {
+                try {
+                    removeCallback(ackEntries, sessionId, index);
+                } finally {
+                    callback.onTimeout();
+                }
+            } finally {
+                releaseScheduledRunnable(this);
+            }
+        }
+
+        public Map<String, AckEntry> getAckEntries() {
+            return ackEntries;
+        }
+
+        public void setAckEntries(Map<String, AckEntry> ackEntries) {
+            this.ackEntries = Preconditions.checkNotNull(ackEntries);
+        }
+
+        public String getSessionId() {
+            return sessionId;
+        }
+
+        public void setSessionId(String sessionId) {
+            this.sessionId = Preconditions.checkNotNull(sessionId);
+        }
+
+        public long getIndex() {
+            return index;
+        }
+
+        public void setIndex(long index) {
+            this.index = index;
+        }
+
+        public AckCallback getCallback() {
+            return callback;
+        }
+
+        public void setCallback(AckCallback callback) {
+            this.callback = Preconditions.checkNotNull(callback);
+        }
+    }
+
+    private static class AckEntry {
+
+        final Map<Long, AckCallback<?>> ackCallbacks = new ConcurrentHashMap<Long, AckCallback<?>>();
+        final AtomicLong ackIndex = new AtomicLong(-1);
+
+        public long addAckCallback(AckCallback<?> callback) {
+            long index = ackIndex.incrementAndGet();
+            ackCallbacks.put(index, callback);
+            return index;
+        }
+
+        public Set<Long> getAckIndexes() {
+            return ackCallbacks.keySet();
+        }
+
+        public AckCallback<?> getAckCallback(long index) {
+            return ackCallbacks.get(index);
+        }
+
+        public AckCallback<?> removeCallback(long index) {
+            return ackCallbacks.remove(index);
+        }
+
+        public void initAckIndex(long index) {
+            ackIndex.compareAndSet(-1, index);
+        }
+
+    }
 }
